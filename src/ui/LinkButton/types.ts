3212d70124a77aef8d8b3b263cef95ff4ee29520
Maksym Maslov <maksym@Maksyms-MacBook-Pro.local>
2023-10-27 16:42:45 +0300

    feat(LinkButton)

---
+++ src/ui/LinkButton/types.ts
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+
+export interface LinkButtonProps {
+    children: ReactNode | string;
+    path: string;
+}
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as Flex } from "./Flex";
 export { default as Heading } from "./Heading";
 export { default as Text } from "./Text";
+export { default as LinkButton } from "./LinkButton";
